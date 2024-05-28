0b605dfaf4fc9de6f61d2c5b4ac957fcad474568
NamK666 <chriscole77@naver.com>
2024-05-28 17:45:45 +0900

    final.js

--- final.js
+++ final.js
@@ -1,5 +1,5 @@
-<!-- 사이드 메뉴 시작 -->
-    <script>
+// 사이드 메뉴 시작 //
+    
         function openMenu() {
             document.querySelector('.sidebar').style.width = "200px"; // 사이드바 넓이
             document.querySelector('.mobileBut').style.display = 'none';
@@ -11,13 +11,10 @@
             document.querySelector('.mobileBut').style.display = 'block';
         }
 
-    </script>
+    
 
-
-
-
-    <!-- 이미지 시작 -->
-    <script>
+    // 이미지 시작 //
+    
         const gallery = document.getElementById('gallery');
         const hiddenImgs = document.getElementById('hiddenImgs');
         const continueEx = document.getElementById('continueEx')
@@ -51,23 +48,21 @@
             });
         });
 
-        // 모달창 닫기 버튼 클릭 이벤트 리스너 추가
+        // 모달창 닫기 버튼 클릭 이벤트 리스너 추가 //
         const closeBtn = document.querySelector(".close");
         closeBtn.addEventListener("click", closeModal);
 
-        // 숨겨진 이미지들에 대해서도 클릭 이벤트 리스너 추가
+        // 숨겨진 이미지들에 대해서도 클릭 이벤트 리스너 추가 //
         const hiddenImages = document.querySelectorAll("#hiddenImgs img");
         hiddenImages.forEach(function(img) {
             img.addEventListener("click", function() {
                 showModal(this.src);
             });
         });
-    </script>
+   
 
-    <!-- 지도 시작 -->
-    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=20719926bb335b7c09a4e4374833cf98">
-    </script>
-    <script>
+    // 지도 시작 //
+    
         var mapContainer = document.getElementById('map');
         var mapOption = {
             center: new kakao.maps.LatLng(37.525818, 126.928446),
@@ -90,31 +85,31 @@
             }
         }
 
-        // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+        // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다 //
         function zoomIn() {
             map.setLevel(map.getLevel() - 1);
         }
-
-        // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+ 
+        // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다 //
         function zoomOut() {
             map.setLevel(map.getLevel() + 1);
         }
 
-        // 마커가 표시될 위치입니다
+        // 마커가 표시될 위치입니다 //
         var markerPosition  = new kakao.maps.LatLng(37.525818, 126.928446);
 
-        // 마커를 생성합니다
+        // 마커를 생성합니다 //
         var marker = new kakao.maps.Marker({
             position: markerPosition
         });
 
-        // 마커가 지도 위에 표시되도록 설정합니다
+        // 마커가 지도 위에 표시되도록 설정합니다 //
         marker.setMap(map);
 
-    </script>
 
-    <!-- sub Modal 창 시작 -->
-    <script>
+
+    // sub Modal 창 시작 //
+
         // 모달창 열기 버튼 가져오기 //
         var subModal = document.getElementById("subModal");
         var subButton = document.getElementById("subButton");
@@ -124,7 +119,7 @@
             subModal.style.display = "block";
         }
 
-        // 모달창 닫기
+        // 모달창 닫기 //
         var subModalClose = document.getElementById("subModalclose");
         subModalClose.onclick = function (){
             subModal.style.display = "none";
@@ -136,10 +131,8 @@
                 subModal.style.display = "none";
             }
         }
-    </script>
 
-    <!-- scroll 버튼 -->
-    <script>
+    // scroll 버튼 //
         const arrowBefore = document.getElementById('arrowbefore');
         const arrowAfter = document.getElementById('arrowafter');
 
@@ -160,4 +153,4 @@
         function scrollToTop() {
             window.scrollTo({top:0, behavior:'smooth'});
         }
-    </script>
+    
